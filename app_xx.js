a0adea68e12274d2b65763fed60d97f192fb6882
Sky00l <207410290@gms.tku.edu.tw>
2023-03-10 23:52:08 +0800

    W04-P3: show all filter buttons into html (section-center)including (all, breakfast, lunch, dinner, shakes) 

--- app_xx.js
+++ app_xx.js
@@ -1,70 +1,69 @@
-const menu = [
-   {
+const menu = [{
     id: 1,
     title: "buttermilk pancakes",
     category: "breakfast",
-    price:15.59,
-    img:'./images/item-1.jpeg',
+    price: 15.59,
+    img: './images/item-1.jpeg',
     remote_img: '',
     desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente dolore ullam ipsam suscipit!'
-   },
-   {
+  },
+  {
     id: 2,
     title: "dinner double",
     category: "dinner",
-    price:25.69,
-    img:'./images/item-2.jpeg',
+    price: 25.69,
+    img: './images/item-2.jpeg',
     remote_img: '',
     desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente dolore ullam ipsam suscipit!'
-   },
-   {
+  },
+  {
     id: 3,
     title: "godzilla milkshake",
     category: "shakes",
-    price:35.49,
-    img:'./images/item-3.jpeg',
+    price: 35.49,
+    img: './images/item-3.jpeg',
     remote_img: '',
     desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente dolore ullam ipsam suscipit!'
-   },
-   {
+  },
+  {
     id: 4,
     title: "cheese sandwich",
     category: "breakfast",
-    price:45.99,
-    img:'./images/my-1.jpg',
+    price: 45.99,
+    img: './images/my-1.jpg',
     remote_img: '',
     desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente dolore ullam ipsam suscipit!'
-   },
-   {
+  },
+  {
     id: 5,
     title: "chocolate ice-cream",
     category: "dessert",
-    price:55.49,
-    img:'./images/my-2.jpg',
+    price: 55.49,
+    img: './images/my-2.jpg',
     remote_img: '',
     desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente dolore ullam ipsam suscipit!'
-   },
-   {
+  },
+  {
     id: 6,
     title: "cheeseburger",
     category: "lunch",
-    price:65.49,
-    img:'./images/my-3.jpg',
+    price: 65.49,
+    img: './images/my-3.jpg',
     remote_img: '',
     desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente dolore ullam ipsam suscipit!'
-   },
+  },
 
 ];
 
-const categories = ['all', 'breakfast', 'lunch', 'dinner', 'shakes'];
+
 
 
 const sectionCenter = document.querySelector('.section-center');
-const btnContainer = document.querySelector('btn-container');
+
 
 const displayMenuItems = (menu) => {
-    let displayMenu = menu.map( (item) => {
-        return `
+  let displayMenu = menu.map((item) => {
+    return `
         <article class="menu-item">
         <img src="${item.img}" alt="${item.title}" class="photo" />
         <div class="item-info">
@@ -77,41 +76,35 @@ const displayMenuItems = (menu) => {
           </p>
         </div>
       </article>`
-      
-    });
-    console.log('displayMenu', displayMenu);
-    displayMenu = displayMenu.join('');
-    console.log('displayMenu after join', displayMenu);
-    sectionCenter.innerHTML = displayMenu;
+
+  });
+  // console.log('displayMenu', displayMenu);
+  displayMenu = displayMenu.join('');
+  // console.log('displayMenu after join', displayMenu);
+  sectionCenter.innerHTML = displayMenu;
+}
+
+
+
+const categories = ['all', 'breakfast', 'lunch', 'dinner', 'shakes'];
+const btnContainer = document.querySelector('btn-container');
+const displayMenuButttons = () => {
+  let menuButtons = categories.map((category) => {
+    return ` <button type="button" class="filter-btn" data-id="${category}">
+    ${category}
+  </button>`;
+  })
+  console.log('displayButttons before join', menuButtons);
+  menuButtons = menuButtons.join('');
+  console.log('displayMenuButtons after join\n', menuButtons);
+  btnContainer.innerHTML = menuButtons;
 }
 
 window.addEventListener('DOMContentLoaded', () => {
-    displayMenuItems(menu);
+  displayMenuItems(menu);
+  displayMenuButttons();
 });
 
 
 
-const displayBtn = (btn) => {
-    let displayBtn = btn.map( (button) => {
-        return `
-         <button type="button" class="filter-btn" data-id="all">${button.category1}</button>
-        <button type="button" class="filter-btn" data-id="breakfast">
-        ${button.category2}
-        </button>
-        <button type="button" class="filter-btn" data-id="lunch">${button.category3}</button>
-        <button type="button" class="filter-btn" data-id="shakes">
-        ${button.category4}
-        </button>
-        <button type="button" class="filter-btn" data-id="dinner">${button.category5}</button>
-        `
-    });
-    console.log('displayBtn', displayBtn);
-    displayBtn = displayBtn.join('');
-    console.log('displayBtn after join', displayBtn);
-    btnContainer.innerHTML = displayBtn;
-}
-
-window.addEventListener('DOMContentLoaded', () => {
-    displayBtn(btn);
-});
 
